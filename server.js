2bf289f2a2b5c876165282c93aeb6fb0d505d630
José Ivan <62190299+jjivanc@users.noreply.github.com>
2022-07-19 15:15:44 +0000

    new test

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+//const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+//server.use('/static', express.static(path.join(__dirname, 'public')))
+// Avoid CORS issue
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+const middlewares = jsonServer.defaults({static: 'public'});
 
 server.use(middlewares);
 server.use(router);
